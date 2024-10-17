f2d5dfb243d307179786be6acc1edf8450dd8131
Suparna <dsouza.supy14@gmail.com>
2024-10-17 13:44:56 -0400

    api response used for contact test

--- tests/contactapi.spec.ts
+++ tests/contactapi.spec.ts
@@ -1,10 +1,10 @@
-import { test, expect, APIRequestContext } from '@playwright/test';
+import { test, expect, APIRequestContext, APIResponse } from '@playwright/test';
 import { ContactPage } from '../src/pages/contactPage';
-import { faker } from '@faker-js/faker';
 
 test.describe('Contact', () => {
   let contactPage: ContactPage;
   let fakerAPI: APIRequestContext;
+  let randomPerson: APIResponse;
 
   test.beforeAll(async ({ playwright}) => {
         fakerAPI = await playwright.request.newContext({
@@ -12,7 +12,8 @@ test.describe('Contact', () => {
     });
 
     const response = await fakerAPI.get('users');
-    console.log( await response.json());
+    const responsebody = await response.json();
+    randomPerson = responsebody[0];
   })
 
   test('Fill contact form and verify success message', async ({ page }) => {
@@ -22,7 +23,12 @@ test.describe('Contact', () => {
     await contactPage.navigate()
 
     //  fill out the input fields and submit
-    await contactPage.submitForm(faker.person.fullName(), faker.internet.email(), faker.phone.number(), faker.lorem.paragraphs(2));
+    await contactPage.submitForm(
+      randomPerson['name'], 
+      randomPerson['email'],
+      randomPerson['phone'],
+      randomPerson['website']
+    );
 
     // verify success message
     await expect(contactPage.successTxt).toHaveText('Thanks for contacting us! We will be in touch with you shortly')
