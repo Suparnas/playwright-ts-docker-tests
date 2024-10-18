18b48dec4cec8a040c6f7ca8b4a0d7606dca4b8d
Suparna <dsouza.supy14@gmail.com>
2024-10-18 14:21:52 -0400

    allure reports deleted and on trace on failire added to config

--- playwright.config.ts
+++ playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     baseURL: 'https://practice.sdetunicorns.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     actionTimeout: 10000,
     video: 'on',
     screenshot: 'on'
